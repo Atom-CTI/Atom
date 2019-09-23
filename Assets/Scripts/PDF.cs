c1938676aa323dd4a7d6994689c230532ca55570
Thiago Dalla Dea <30993134+thiagodalladea@users.noreply.github.com>
2019-09-23 15:39:40 -0300

    Enviar email com PDF's ao usuário

--- Assets/Scripts/PDF.cs
+++ Assets/Scripts/PDF.cs
@@ -2,9 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Networking;
+
 
 public class PDF : MonoBehaviour
 {
+    string verif;
+    string email;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,8 +29,37 @@ public class PDF : MonoBehaviour
         SceneManager.LoadScene("Inicio");
     }
 
-    public void apertar()
+    public void Download()
+    {
+        Debug.Log("thiago1");
+        StartCoroutine(BtnDownload());
+    }
+
+    public IEnumerator BtnDownload()
     {
         Debug.Log("thiago");
+
+        string param_url = "http://200.145.153.172/atom/enviarEmail.php?";
+
+        Debug.Log(param_url);
+
+        using (UnityWebRequest url = UnityWebRequest.Get(param_url))
+        {
+            yield return url.Send();
+            string[] szSplited = url.downloadHandler.text.Split(',');
+
+            verif = szSplited[0];
+            email = szSplited[1];
+
+            if (verif == "0")
+            {
+                Debug.Log(email);
+            }
+            else
+            {
+                Debug.Log(email);
+                SceneManager.LoadScene("Inicio");
+            }
+        }
     }
 }
